0b163c529ca6524ea355dfec234e291c62afd156
MarikaP <marika@somatone.com>
2024-04-14 15:56:42 -0500

    Main Menu UI basics + scene transition

---
+++ LudumDare55/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadEndcene()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
